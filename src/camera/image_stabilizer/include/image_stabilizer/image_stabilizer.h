a38acb3137fe1578c1948d54585cfea1089ce370
Valentin Bezzubov <valentin.bezzubov@yandex.ru>
2019-06-02 17:26:24 +0300

    Added simple camera stabilization based on Kalman filter

--- src/camera/image_stabilizer/include/image_stabilizer/image_stabilizer.h
+++ src/camera/image_stabilizer/include/image_stabilizer/image_stabilizer.h
@@ -2,25 +2,97 @@
 
 #include <image_transport/image_transport.h>
 #include <opencv2/imgproc/imgproc.hpp>
+#include "opencv2/video/tracking.hpp"
 #include <cv_bridge/cv_bridge.h>
+
+
+struct TransformParam
+{
+    TransformParam() {}
+    TransformParam(double _dx, double _dy, double _da) :
+      dx(_dx), dy(_dy), da(_da) {}
+
+    double dx;
+    double dy;
+    double da; // angle
+};
+
+struct Trajectory
+{
+    Trajectory() {}
+    Trajectory(double _x, double _y, double _a) :
+      x(_x), y(_y), a(_a) {}
+	// "+"
+	friend Trajectory operator+(const Trajectory &c1,const Trajectory  &c2){
+        return Trajectory(c1.x+c2.x,c1.y+c2.y,c1.a+c2.a);
+	}
+	//"-"
+	friend Trajectory operator-(const Trajectory &c1,const Trajectory  &c2){
+		return Trajectory(c1.x-c2.x,c1.y-c2.y,c1.a-c2.a);
+	}
+	//"*"
+	friend Trajectory operator*(const Trajectory &c1,const Trajectory  &c2){
+		return Trajectory(c1.x*c2.x,c1.y*c2.y,c1.a*c2.a);
+	}
+	//"/"
+	friend Trajectory operator/(const Trajectory &c1,const Trajectory  &c2){
+		return Trajectory(c1.x/c2.x,c1.y/c2.y,c1.a/c2.a);
+	}
+	//"="
+	Trajectory operator =(const Trajectory &rx){
+		x = rx.x;
+		y = rx.y;
+		a = rx.a;
+		return Trajectory(x,y,a);
+	}
+
+    double x;
+    double y;
+    double a; // angle
+};
 
 
 class ImageStabilizer
 {
-    ros::NodeHandle nh_;
-    image_transport::ImageTransport it_;
-    image_transport::Subscriber image_sub_;
-    image_transport::Publisher image_pub_;
+    ros::NodeHandle nh;
+    image_transport::ImageTransport it;
+    image_transport::Subscriber image_sub;
+    image_transport::Publisher image_pub;
 
-    sensor_msgs::ImageConstPtr curr_image;
+    const int HORIZONTAL_BORDER_CROP = 20;
 
-    cv::Mat curr_mat, prev_mat;
-    cv::Mat curr_mat_gray, prev_mat_gray;
+    cv::Mat curr_gray, prev, prev_gray;
+	cv::Mat T, last_T;
+
+    int vert_border;
+
+    std::vector <TransformParam> prev_to_cur_transform; 
+
+	double a;
+	double x;
+	double y;
+
+	std::vector <Trajectory> trajectory; 
+
+	std::vector <Trajectory> smoothed_trajectory; 
+	Trajectory X;
+	Trajectory X_;
+	Trajectory P;
+	Trajectory P_;
+	Trajectory K;
+	Trajectory z;
+
+	double pstd = 4e-3;
+	double cstd = 0.025;
+	Trajectory Q;
+	Trajectory R;
+
+	std::vector <TransformParam> new_prev_to_cur_transform;
 
     void image_callback(const sensor_msgs::ImageConstPtr& msg);
-    cv::Mat stabilize_image(const cv::Mat& img);
+    cv_bridge::CvImagePtr ros_to_cv(const sensor_msgs::ImageConstPtr& image);
+    cv::Mat stabilize_image(const cv::Mat& curr);
 
     public:
         ImageStabilizer(std::string sub_topic, std::string pub_topic);
-        sensor_msgs::ImageConstPtr get_curr_image() const {return curr_image;};
 };
--- src/camera/image_stabilizer/src/image_stabilizer.cpp
+++ src/camera/image_stabilizer/src/image_stabilizer.cpp
@@ -1,29 +1,114 @@
 #include "image_stabilizer/image_stabilizer.h"
 
 
-ImageStabilizer::ImageStabilizer(std::string sub_topic, std::string pub_topic) : it_(nh_)
+ImageStabilizer::ImageStabilizer(std::string sub_topic, std::string pub_topic) : it(nh), 
+                                                                                 x(0), y(0), a(0), 
+                                                                                 Q(pstd,pstd,pstd), R(cstd,cstd,cstd),
+                                                                                 T(2, 3, CV_64F),
+                                                                                 X(0, 0, 0), P(1, 1, 1)
 {
-    image_sub_ = it_.subscribe(sub_topic, 1, &ImageStabilizer::image_callback, this);
-    image_pub_ = it_.advertise(pub_topic, 1);
+    image_sub = it.subscribe(sub_topic, 1, &ImageStabilizer::image_callback, this);
+    image_pub = it.advertise(pub_topic, 1);
+    sensor_msgs::ImageConstPtr first_img_ptr = ros::topic::waitForMessage<sensor_msgs::Image>(sub_topic);
+    prev = ros_to_cv(first_img_ptr)->image;
+    cv::cvtColor(prev, prev_gray, cv::COLOR_BGR2GRAY);
+    vert_border = HORIZONTAL_BORDER_CROP * prev.rows / prev.cols;
 }
 
 
 void ImageStabilizer::image_callback(const sensor_msgs::ImageConstPtr& msg)
 {
-    cv_bridge::CvImagePtr cv_ptr;
-    try
-    {
-        cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
-    }
-    catch (cv_bridge::Exception& e)
-    {
-      ROS_ERROR("cv_bridge exception: %s", e.what());
-      return;
-    }
-    image_pub_.publish(cv_ptr->toImageMsg());
+    cv_bridge::CvImagePtr cv_ptr = ros_to_cv(msg);
+    cv_ptr->image = stabilize_image(cv_ptr->image);
+    image_pub.publish(cv_ptr->toImageMsg());
 }
 
 
-cv::Mat ImageStabilizer::stabilize_image(const cv::Mat& img)
+cv_bridge::CvImagePtr ImageStabilizer::ros_to_cv(const sensor_msgs::ImageConstPtr& image)
 {
+    cv_bridge::CvImagePtr cv_ptr;
+    try
+    {
+        cv_ptr = cv_bridge::toCvCopy(image, sensor_msgs::image_encodings::BGR8);
+    }
+    catch (cv_bridge::Exception& e)
+    {
+        ROS_ERROR("cv_bridge exception: %s", e.what());
+        return cv_bridge::CvImagePtr();
+    }
+    return cv_ptr;
+}
+
+
+cv::Mat ImageStabilizer::stabilize_image(const cv::Mat& curr)
+{
+    cv::cvtColor(curr, curr_gray, cv::COLOR_BGR2GRAY);
+
+	std::vector <cv::Point2f> prev_corner_temp, curr_corner_temp;
+    std::vector <cv::Point2f> prev_corner, curr_corner;
+    std::vector <uchar> status;
+    std::vector <float> err;
+
+    cv::goodFeaturesToTrack(prev_gray, prev_corner_temp, 200, 0.01, 30);
+    cv::calcOpticalFlowPyrLK(prev_gray, curr_gray, prev_corner_temp, curr_corner_temp, status, err);
+
+	for(size_t i = 0; i < status.size(); i++) 
+    {
+        if(status[i]) 
+        {
+            prev_corner.push_back(prev_corner_temp[i]);
+            curr_corner.push_back(curr_corner_temp[i]);
+        }
+    }
+
+    T = cv::estimateRigidTransform(prev_corner, curr_corner, false); // false = rigid transform, no scaling/shearing
+
+    if(T.data == NULL) 
+        last_T.copyTo(T);
+    T.copyTo(last_T);
+
+    double dx = T.at<double>(0,2);
+    double dy = T.at<double>(1,2);
+    double da = atan2(T.at<double>(1,0), T.at<double>(0,0));
+
+    x += dx;
+    y += dy;
+    a += da;
+
+    z = Trajectory(x,y,a);
+    X_ = X; //X_(k) = X(k-1);
+    P_ = P+Q; //P_(k) = P(k-1)+Q;
+
+    K = P_/( P_ + R ); //gain;K(k) = P_(k)/( P_(k)+R );
+    X = X_+K * (z - X_); //z-X_ is residual,X(k) = X_(k)+K(k)*(z(k)-X_(k)); 
+    P = (Trajectory(1,1,1)-K) * P_; //P(k) = (1-K(k))*P_(k);
+
+    double diff_x = X.x - x;
+	double diff_y = X.y - y;
+	double diff_a = X.a - a;
+
+	dx = dx + diff_x;
+	dy = dy + diff_y;
+	da = da + diff_a;
+
+	T.at<double>(0,0) = cos(da);
+	T.at<double>(0,1) = -sin(da);
+	T.at<double>(1,0) = sin(da);
+	T.at<double>(1,1) = cos(da);
+
+	T.at<double>(0,2) = dx;
+	T.at<double>(1,2) = dy;
+
+	cv::Mat curr2;
+	
+	cv::warpAffine(prev, curr2, T, curr.size());
+
+	curr2 = curr2(cv::Range(vert_border, curr2.rows - vert_border), cv::Range(HORIZONTAL_BORDER_CROP, curr2.cols - HORIZONTAL_BORDER_CROP));
+
+	cv::resize(curr2, curr2, curr.size());
+
+    curr.copyTo(prev);
+	curr_gray.copyTo(prev_gray);
+
+    return curr2;
 }
